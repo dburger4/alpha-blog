af7ab4f91bde8e9d3f789ce0da282e3abb16b666
Dan Burger <burger5@purdue.edu>
2020-06-02 13:42:22 -0400

    add authentication helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:iser_id] = nil
+    session[:user_id] = nil
     flash[:notice] = "Logged out"
     redirect_to root_path
   end
